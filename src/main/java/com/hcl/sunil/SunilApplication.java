7e64b8d7a8490350ac47a2a8170ccc40213c28bc
Sunil1984 <sunilslate@gmail.com>
2019-07-03 00:27:31 +0530

    coded

---
+++ src/main/java/com/hcl/sunil/SunilApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.sunil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunilApplication.class, args);
+	}
+
+}
